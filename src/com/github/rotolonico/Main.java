a7a3d89e82b41715f58cac0ca490d437d1e3cc7c
DomeNico Rotolo <rotolonico@gmail.com>
2017-06-02 07:51:29 +0200

    If you enter an illegal value now there is no exception! YEY!

--- src/com/github/rotolonico/Main.java
+++ src/com/github/rotolonico/Main.java
@@ -10,29 +10,31 @@ public class Main {
     }
 
     public void init() {
-        Scanner s = new Scanner(System.in);
-        System.out.println("Insert max 2 powered value ");
-        int numbergiven = s.nextInt();
-        Power2(numbergiven);
-
-    }
-    public void Power2(int numbergiven) {
         try {
 
-            int number = 1;
-            if (numbergiven > 1073741823) {
-                Power2(1073741823);
-                System.out.println("1073741824");
-            } else {
-                while (number <= numbergiven) {
-                    System.out.println(number);
-                    number = number * 2;
-                }
-            }
-
-        } catch (InputMismatchException e){
+            Scanner s = new Scanner(System.in);
+            System.out.println("Insert max 2 powered value ");
+            int numbergiven = s.nextInt();
+            Power2(numbergiven);
+        } catch (InputMismatchException e) {
             System.out.println("This is not a legal number");
             init();
         }
+    }
+
+    public void Power2(int numbergiven) {
+
+        int number = 1;
+        if (numbergiven > 1073741823) {
+            Power2(1073741823);
+            System.out.println("1073741824");
+        } else {
+            while (number <= numbergiven) {
+                System.out.println(number);
+                number = number * 2;
+            }
+        }
+
+
     }
 }
