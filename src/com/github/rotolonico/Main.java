c8ad958e63852f907881b810d821fc7e76fca413
DomeNico Rotolo <rotolonico@gmail.com>
2017-06-02 07:14:51 +0200

    First Commit

---
+++ src/com/github/rotolonico/Main.java
@@ -0,0 +1,32 @@
+package com.github.rotolonico;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        new Main().init();
+    }
+
+    public void init() {
+        Power2();
+
+    }
+    public void Power2() {
+        try {
+
+            Scanner s = new Scanner(System.in);
+            System.out.print("Insert max 2 powered value ");
+            int numbergiven = s.nextInt();
+            int number = 1;
+            while (number<=numbergiven){
+                System.out.println(number);
+                number = number*2;
+            }
+        } catch (InputMismatchException e){
+            System.out.println("This is not a legal number");
+            init();
+        }
+    }
+}
