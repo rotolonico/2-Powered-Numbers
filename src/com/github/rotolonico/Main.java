98a0d225c7c8d4c6110dc1ea75e013d77bbe8b45
DomeNico Rotolo <rotolonico@gmail.com>
2017-06-02 08:39:39 +0200

    You can now select the base! Huge change!

--- src/com/github/rotolonico/Main.java
+++ src/com/github/rotolonico/Main.java
@@ -13,26 +13,36 @@ public class Main {
         try {
 
             Scanner s = new Scanner(System.in);
-            System.out.println("Insert max 2 powered value ");
+            System.out.println("Insert max value: ");
             int numbergiven = s.nextInt();
-            Power2(numbergiven);
+            System.out.println("Insert base: ");
+            int p = s.nextInt();
+            Power2(numbergiven, p);
         } catch (InputMismatchException e) {
             System.out.println("This is not a legal number");
             init();
         }
     }
 
-    public void Power2(int numbergiven) {
+    public void Power2(int numbergiven, int p) {
 
         int number = 1;
         if (numbergiven > 1073741823) {
-            Power2(1073741823);
-            System.out.println("1073741824");
+            Power2(1073741823, p);
         } else {
-            while (number <= numbergiven) {
-                System.out.println(number);
-                number = number * 2;
+            if (p == 0) {
+                System.out.println("0");
+            } else if (p == 1){
+                System.out.println("1");
+            } else {
+                while (number <= numbergiven) {
+                    number = number * p;
+                    if (number <= numbergiven) {
+                        System.out.println(number);
+                    }
+                }
             }
+
         }
 
 
