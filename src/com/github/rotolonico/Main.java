84177ea011ff16e9a9d71684e3b8873b1ece3690
DomeNico Rotolo <rotolonico@gmail.com>
2017-06-02 07:43:28 +0200

    Fixed a bug occurring with big numbers

--- src/com/github/rotolonico/Main.java
+++ src/com/github/rotolonico/Main.java
@@ -10,20 +10,26 @@ public class Main {
     }
 
     public void init() {
-        Power2();
+        Scanner s = new Scanner(System.in);
+        System.out.println("Insert max 2 powered value ");
+        int numbergiven = s.nextInt();
+        Power2(numbergiven);
 
     }
-    public void Power2() {
+    public void Power2(int numbergiven) {
         try {
 
-            Scanner s = new Scanner(System.in);
-            System.out.println("Insert max 2 powered value ");
-            int numbergiven = s.nextInt();
             int number = 1;
-            while (number<=numbergiven){
-                System.out.println(number);
-                number = number*2;
+            if (numbergiven > 1073741823) {
+                Power2(1073741823);
+                System.out.println("1073741824");
+            } else {
+                while (number <= numbergiven) {
+                    System.out.println(number);
+                    number = number * 2;
+                }
             }
+
         } catch (InputMismatchException e){
             System.out.println("This is not a legal number");
             init();
